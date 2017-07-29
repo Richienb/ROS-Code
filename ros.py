6803bea4076d59fcf072969eb2401ab335e171b9
Richienb <richiebendall@gmail.com>
2017-07-29 18:31:34 +1200

    Changed error_print to error

--- ros.py
+++ ros.py
@@ -15,7 +15,7 @@ def multiply_print(firstnum, secondnum):
 def divide_print(firstnum, secondnum):
     print(firstnum / secondnum)
 
-def error_print(text):
+def error(text):
     raise RuntimeError(text)
   
 print('Finished Setting Up ROS Code')
