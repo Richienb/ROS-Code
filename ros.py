c75f4e8180499f6a2b86f7da7e5e31966bfb8b79
Richienb <richiebendall@gmail.com>
2017-07-29 18:45:32 +1200

    Added Writable Equations
    Added Add_Write
    Added Subtract_Write
    Added Multiply_Write
    Added Divide_Write

--- ros.py
+++ ros.py
@@ -15,6 +15,22 @@ def multiply_print(firstnum, secondnum):
 def divide_print(firstnum, secondnum):
     print(firstnum / secondnum)
 
+def add_write(firstnum, secondnum):
+    ros_output = (firstnum + secondnum)
+    global ros_output
+
+def subtract_write(firstnum, secondnum):
+    ros_output = (firstnum - secondnum)
+    global ros_output
+
+def multiply_write(firstnum, secondnum):
+    ros_output = (firstnum * secondnum)
+    global ros_output
+
+def divide_write(firstnum, secondnum):
+    ros_output = (firstnum / secondnum)
+    global ros_output
+
 def error(text):
     raise RuntimeError(text)
   
