583ff468218e8b49056e0dda64c115cf49fc95c0
Richienb <richiebendall@gmail.com>
2017-08-01 19:48:51 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 
-setup(
-	name='ros',    # This is the name of your PyPI-package.
-	version='0.1',                          # Update the version number for new releases
-	scripts=['ros']                  # The name of your scipt, and also the command you'll be using for calling it
-)
+setup(name='ros',
+      version='0.1',
+      description='The Easiest Way To Code Easily.',
+      url='https://github.com/Richienb/ROS-Code',
+      author='Richie Bendall',
+      author_email='richiebendall@gmail.com',
+      license='Apache-2.0',
+      scripts=['ros.py'],
+      zip_safe=False)
