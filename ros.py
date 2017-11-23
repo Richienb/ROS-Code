b174047d0dbbfc6f833dace190ca934fc31e25fa
Richie Bendall <richiebendall@gmail.com>
2017-11-23 19:47:06 +1300

    Added converttime(time, currentformat, newformat)

--- ros.py
+++ ros.py
@@ -615,6 +615,227 @@ def reversetext(text, ignoretype=False):
 	else:
 		return str(text)[::-1]
 		
+# Convert A Time Peroid To Another One
+def converttime(time, currentformat, newformat):
+	currentformat = currentformat.lower()
+	newformat = newformat.lower()
+	if currentformat == newformat:
+		return time
+	if currentformat == 'seconds':
+		if newformat == 'milliseconds':
+			return time * 1000
+		elif newformat == 'minutes':
+			return time / 60
+		elif newformat == 'hours':
+			return time / 60 / 60
+		elif newformat == 'days':
+			return time / 60 / 60 / 24
+		elif newformat == 'weeks':
+			return time / 60 / 60 / 24 / 7
+		elif newformat == 'fortnights':
+			return time / 60 / 60 / 24 / 14
+		elif newformat == 'years':
+			return time / 60 / 60 / 24 / 365
+		elif newformat == 'decades':
+			return time / 60 / 60 / 24 / 365 / 10
+		elif newformat == 'centuaries':
+			return time / 60 / 60 / 24 / 365 / 100
+		elif newformat == 'millenniums':
+			return time / 60 / 60 / 24 / 365 / 1000
+	elif currentformat == 'minutes':
+		if newformat == 'milliseconds':
+			return time * 60 * 1000
+		elif newformat == 'seconds':
+			return time * 60
+		elif newformat == 'hours':
+			return time / 60
+		elif newformat == 'days':
+			return time / 60 / 24
+		elif newformat == 'weeks':
+			return time / 60 / 24 / 7
+		elif newformat == 'fortnights':
+			return time / 60 / 24 / 14
+		elif newformat == 'years':
+			return time / 60 / 24 / 365
+		elif newformat == 'decades':
+			return time / 60 / 24 / 365 / 10
+		elif newformat == 'centuaries':
+			return time / 60 / 24 / 365 / 100
+		elif newformat == 'millenniums':
+			return time / 60 / 24 / 365 / 1000
+	elif currentformat == 'hours':
+		if newformat == 'milliseconds':
+			return time * 60 * 60 * 1000
+		elif newformat == 'seconds':
+			return time * 60 * 60
+		elif newformat == 'minutes':
+			return time / 60
+		elif newformat == 'days':
+			return time / 24
+		elif newformat == 'weeks':
+			return time / 24 / 7
+		elif newformat == 'fortnights':
+			return time / 24 / 14
+		elif newformat == 'years':
+			return time / 24 / 7 / 365
+		elif newformat == 'decades':
+			return time / 24 / 365 / 10
+		elif newformat == 'centuaries':
+			return time / 24 / 365 / 100
+		elif newformat == 'millenniums':
+			return time / 24 / 365 / 1000
+	elif currentformat == 'days':
+		if newformat == 'milliseconds':
+			return time * 24 * 60 * 60 * 1000
+		elif newformat == 'seconds':
+			return time * 24 * 60 * 60
+		elif newformat == 'minutes':
+			return time * 24 * 60
+		elif newformat == 'hours':
+			return time * 24
+		elif newformat == 'weeks':
+			return time / 24 / 7
+		elif newformat == 'fortnights':
+			return time / 60 / 24 / 14
+		elif newformat == 'years':
+			return time / 60 / 24 / 7 / 365
+		elif newformat == 'decades':
+			return time / 7 / 365 / 10
+		elif newformat == 'centuaries':
+			return time / 7 / 365 / 100
+		elif newformat == 'millenniums':
+			return time / 7 / 365 / 1000
+	elif currentformat == 'weeks':
+		if newformat == 'milliseconds':
+			return time * 7 * 24 * 60 * 60 * 1000
+		elif newformat == 'seconds':
+			return time * 7 * 24 * 60 * 60
+		elif newformat == 'minutes':
+			return time * 7 * 24 * 60
+		elif newformat == 'hours':
+			return time * 7 * 24
+		elif newformat == 'fortnights':
+			return time * 7 / 14
+		elif newformat == 'years':
+			return time * 7 / 365
+		elif newformat == 'decades':
+			return time * 7 / 365 / 10
+		elif newformat == 'centuaries':
+			return time * 7 / 365 / 100
+		elif newformat == 'millenniums':
+			return time * 7 / 365 / 1000
+	elif currentformat == 'fortnights':
+		if newformat == 'milliseconds':
+			return time * 14 * 24 * 60 * 60 * 1000
+		elif newformat == 'seconds':
+			return time * 14 * 24 * 60 * 60
+		elif newformat == 'minutes':
+			return time * 14 * 24 * 60
+		elif newformat == 'hours':
+			return time * 14 * 24
+		elif newformat == 'weeks':
+			return time * 14
+		elif newformat == 'years':
+			return time * 14 / 365
+		elif newformat == 'decades':
+			return time * 14 / 365 / 10
+		elif newformat == 'centuaries':
+			return time * 14 / 365 / 100
+		elif newformat == 'millenniums':
+			return time * 14 / 365 / 1000
+	elif currentformat == 'years':
+		if newformat == 'milliseconds':
+			return time * 365 * 24 * 60 * 60 * 1000
+		elif newformat == 'seconds':
+			return time * 365 * 24 * 60 * 60
+		elif newformat == 'minutes':
+			return time * 365 * 24 * 60
+		elif newformat == 'hours':
+			return time * 365 * 24
+		elif newformat == 'days':
+			return time * 365
+		elif newformat == 'weeks':
+			return time * 365 / 7
+		elif newformat == 'fortnights':
+			return time * 365 / 14
+		elif newformat == 'decades':
+			return time / 10
+		elif newformat == 'centuaries':
+			return time / 100
+		elif newformat == 'millenniums':
+			return time / 1000
+	elif currentformat == 'decades':
+		if newformat == 'milliseconds':
+			return time * 10 * 365 * 24 * 60 * 60 * 1000
+		elif newformat == 'seconds':
+			return time * 10 * 365 * 24 * 60 * 60
+		elif newformat == 'minutes':
+			return time * 10 * 365 * 24 * 60
+		elif newformat == 'hours':
+			return time * 10 * 365 * 24
+		elif newformat == 'days':
+			return time * 10 * 365
+		elif newformat == 'weeks':
+			return time *10 * 365 / 7
+		elif newformat == 'fortnights':
+			return time * 10 * 365 / 14
+		elif newformat == 'years':
+			return time * 10
+		elif newformat == 'centuaries':
+			return time * 10 / 100
+		elif newformat == 'millenniums':
+			return time * 10 / 1000
+	elif currentformat == 'centuaries':
+		if newformat == 'milliseconds':
+			return time * 100 * 365 * 24 * 60 * 60 * 1000
+		elif newformat == 'seconds':
+			return time * 100 * 365 * 24 * 60 * 60
+		elif newformat == 'minutes':
+			return time * 100 * 365 * 24 * 60
+		elif newformat == 'hours':
+			return time * 100 * 365 * 24
+		elif newformat == 'days':
+			return time * 100 * 365
+		elif newformat == 'weeks':
+			return time *100 * 365 / 7
+		elif newformat == 'fortnights':
+			return time * 100 * 365 / 14
+		elif newformat == 'years':
+			return time * 100
+		elif newformat == 'decades':
+			return time * 100 / 10
+		elif newformat == 'millenniums':
+			return time * 100 / 1000
+	elif currentformat == 'millenniums':
+		if newformat == 'milliseconds':
+			return time * 1000 * 365 * 24 * 60 * 60 * 1000
+		elif newformat == 'seconds':
+			return time * 1000 * 365 * 24 * 60 * 60
+		elif newformat == 'minutes':
+			return time * 1000 * 365 * 24 * 60
+		elif newformat == 'hours':
+			return time * 1000 * 365 * 24
+		elif newformat == 'days':
+			return time * 1000 * 365
+		elif newformat == 'weeks':
+			return time *1000 * 365 / 7
+		elif newformat == 'fortnights':
+			return time * 1000 * 365 / 14
+		elif newformat == 'years':
+			return time * 1000
+		elif newformat == 'decades':
+			return time * 1000 / 10
+		elif newformat == 'centuaries':
+			return time * 1000 / 100
+		
+# Reverse A List
+def reverselist(list):
+	return list.reverse()
+	
+# Replace Text In A Variable
+def replacetext(string, texttofind, texttoreplace):
+	return string.replace(texttofind, texttoreplace)
+
 # Convert A Base 10 Number To A Custom Base
 def convertbase(number, base=10):
 	import string
