ab188b6056089d8a9fa61139189359a7a8cf31d7
Richie Bendall <richiebendall@gmail.com>
2017-11-25 16:29:36 +1300

    Added getdatetime(timedateformat='complete')

--- ros.py
+++ ros.py
@@ -1030,6 +1030,39 @@ def randomstr(valuelist):
 def timesince():
 	from time import time as time_now
 	return time_now()
+
+# Get The Current Date Or Time
+def getdatetime(timedateformat='complete'):
+	from datetime import datetime
+	timedateformat = timedateformat.lower()
+	if timedateformat == 'day':
+		return ((str(datetime.now())).split(' ')[0]).split('-')[2]
+	elif timedateformat == 'month':
+		return ((str(datetime.now())).split(' ')[0]).split('-')[1]
+	elif timedateformat == 'year':
+		return ((str(datetime.now())).split(' ')[0]).split('-')[0]
+	elif timedateformat == 'hour':
+		return (((str(datetime.now())).split(' ')[1]).split('.')[0]).split(':')[0]
+	elif timedateformat == 'minute':
+		return (((str(datetime.now())).split(' ')[1]).split('.')[0]).split(':')[1]
+	elif timedateformat == 'second':
+		return (((str(datetime.now())).split(' ')[1]).split('.')[0]).split(':')[2]
+	elif timedateformat == 'millisecond':
+		return (str(datetime.now())).split('.')[1]
+	elif timedateformat == 'yearmonthday':
+		return (str(datetime.now())).split(' ')[0]
+	elif timedateformat == 'daymonthyear':
+		return ((str(datetime.now())).split(' ')[0]).split('-')[2] + '-' + ((str(datetime.now())).split(' ')[0]).split('-')[1] + '-' + ((str(datetime.now())).split(' ')[0]).split('-')[0]
+	elif timedateformat == 'hourminutesecond':
+		return ((str(datetime.now())).split(' ')[1]).split('.')[0]
+	elif timedateformat == 'secondminutehour':
+		return (((str(datetime.now())).split(' ')[1]).split('.')[0]).split(':')[2] + ':' + (((str(datetime.now())).split(' ')[1]).split('.')[0]).split(':')[1] + ':' + (((str(datetime.now())).split(' ')[1]).split('.')[0]).split(':')[0]
+	elif timedateformat == 'complete':
+		return str(datetime.now())
+	elif timedateformat == 'datetime':
+		return (str(datetime.now())).split('.')[0]
+	elif timedateformat == 'timedate':
+		return ((str(datetime.now())).split('.')[0]).split(' ')[1] + ' ' + ((str(datetime.now())).split('.')[0]).split(' ')[0]	
 	
 # Get User input
 def userinput(prompttext):
@@ -1037,7 +1070,7 @@ def userinput(prompttext):
 		return input(prompttext)
 	except:
 		return input(str(prompttext))
-
+		
 # Licence Information
 def licence(raw=False):
 	if raw == False:
