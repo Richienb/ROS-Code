bd39bd69d1ac7b2193c8edd62c98d68747992e5f
Richie Bendall <richiebendall@gmail.com>
2018-04-03 18:30:54 +1200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import setup
-
-setup(name='ros',
-      version='0.1',
-      description='The Easiest Way To Code Easily.',
-      url='https://github.com/Richienb/ROS-Code',
-      author='Richie Bendall',
-      author_email='richiebendall@gmail.com',
-      license='Apache-2.0',
-      scripts=['ros.py'],
-      zip_safe=False)
+from distutils.core import setup
+setup(
+  name = 'ROS Code',
+  packages = ['ros.py'],
+  version = '0.1',
+  description = 'ROS Code is a revolutionary new way to code featuring a fast installation, a simple syntax and is provided as a python library.',
+  author = 'Richie Bendall',
+  author_email = 'richiebendall@gmail.com',
+  url = 'https://github.com/Richienb/ROS-Code',
+  download_url = 'https://github.com/Richienb/ROS-Code/archive/master.zip',
+  keywords = ['ros', 'code', 'ros code', 'python', 'library'],
+  classifiers = [],
+)
