26cf12f0e9f4f818ffd092b8958a31e74106793a
Richie Bendall <richiebendall@gmail.com>
2018-06-26 18:09:00 +1200

    Tidy test_syntax

--- src/test_syntax.py
+++ src/test_syntax.py
@@ -1,6 +1,7 @@
 import unittest as ut
 import syntax as s
 
+
 class TestCode(ut.TestCase):
 
     def test_colourcode(self):
@@ -10,7 +11,8 @@ class TestCode(ut.TestCase):
         self.assertEqual(str(s.changecolour('#212121', 'red', 10)), '#192121')
 
     def test_catwalk(self):
-        self.assertEqual(s.catwalk('this     is    some    text'), 'this is some text')
+        self.assertEqual(
+            s.catwalk('this     is    some    text'), 'this is some text')
 
     def test_isprime(self):
         self.assertEqual(s.isprime(1), False)
@@ -25,14 +27,18 @@ class TestCode(ut.TestCase):
         self.assertEqual(s.absolutenum(3 - 4j), 5.0)
 
     def test_splitstring(self):
-        self.assertEqual(s.splitstring('hello my name'), ['hello', 'my', 'name'])
-        self.assertEqual(s.splitstring('hello my name', '-'), ['hello my name'])
-        self.assertEqual(s.splitstring('hello-my-name', '-'), ['hello', 'my', 'name'])
+        self.assertEqual(s.splitstring('hello my name'),
+                         ['hello', 'my', 'name'])
+        self.assertEqual(s.splitstring(
+            'hello my name', '-'), ['hello my name'])
+        self.assertEqual(s.splitstring('hello-my-name', '-'),
+                         ['hello', 'my', 'name'])
         self.assertEqual(s.splitstring('hello-my-name', '-', 0), 'hello')
 
     def test_sort(self):
         self.assertEqual(s.sort(['d', 'a', 'c', 'b']), ['a', 'b', 'c', 'd'])
-        self.assertEqual(s.sort(['d', 'a', 'c', 'b'], None, True), ['d', 'c', 'b', 'a'])
+        self.assertEqual(s.sort(['d', 'a', 'c', 'b'], None, True), [
+                         'd', 'c', 'b', 'a'])
 
     def test_pykeyword(self):
         import keyword
@@ -54,7 +60,7 @@ class TestCode(ut.TestCase):
         self.assertEqual(s.isfib(385), False)
 
     def test_psrgame(self):
-        results =  ["Tie", "Win", "Loose"]
+        results = ["Tie", "Win", "Loose"]
         self.assertEqual(s.psrgame("paper") in results, True)
         self.assertEqual(s.psrgame("scissor") in results, True)
         self.assertEqual(s.psrgame("rock") in results, True)
@@ -103,6 +109,7 @@ class TestCode(ut.TestCase):
         self.assertEqual(s.equation("multiply", 56, 15), 840)
         self.assertEqual(s.equation("divide", 5, 2), 2.5)
         self.assertEqual(s.equation("divide", 125, 5), 25)
+
 
 if __name__ == '__main__':
     ut.main()
