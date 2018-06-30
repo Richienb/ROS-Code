71397891689063a73c141c98fb70da8326925022
Richie Bendall <richiebendall@gmail.com>
2018-07-01 00:41:40 +1200

    Update syntax.py

--- src/syntax.py
+++ src/syntax.py
@@ -2,7 +2,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 from future import standard_library
 standard_library.install_aliases()
-from builtins import bytes, dict, int, list, object, range, str, ascii, chr, hex, input, next, oct, open, pow, round, super, filter, map, zip
+from builtins import ( bytes, dict, int, list, object, range, str, ascii, chr, hex, 
+                      input, next, oct, open, pow, round, super, filter, map, zip )
 
 # System modules
 from subprocess import call
