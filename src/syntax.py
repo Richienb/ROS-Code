99e305e37bb9f6823521765cbe19928175a9b225
Richie Bendall <richiebendall@gmail.com>
2018-07-01 00:18:18 +1200

    Update syntax.py

--- src/syntax.py
+++ src/syntax.py
@@ -1,7 +1,7 @@
 # Backwards Compatibility
+from __future__ import absolute_import, division, print_function, unicode_literals
 from future import standard_library
 standard_library.install_aliases()
-from __future__ import absolute_import, division, print_function, unicode_literals
 from builtins import bytes, dict, int, list, object, range, str, ascii, chr, hex, input, next, oct, open, pow, round, super,filter, map, zip
 
 # System modules
