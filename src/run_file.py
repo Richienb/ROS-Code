58572238522be547c69a57f08f17b0a77d307a06
Richie Bendall <richiebendall@gmail.com>
2018-07-02 22:57:15 +1200

    Update run_file.py

--- src/run_file.py
+++ src/run_file.py
@@ -34,10 +34,10 @@ with open(args[1]) as f:
             lastprinttext = printtext.replace(")", "')")
             try:
                 exec(str("syntax." + lastprinttext))
-            except Exception as E:
+            except Exception as exc:
                 template = "ERROR: An error of type {0} occured while running line {1} because {2}"
                 message = template.format(
-                    type(e).__name__, str(value[0] + 1), str(E.args[0]))
+                    type(exc).__name__, str(value[0] + 1), str(exc.args[0]))
                 print(message)
                 exitexc(1)
         elif value[1].startswith('!!!'):
