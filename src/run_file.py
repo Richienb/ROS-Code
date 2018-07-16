aaddf090a4aa5d4009441cad3d235e2fbb219f17
Richie Bendall <richiebendall@gmail.com>
2018-07-16 13:44:57 +1200

    Update run_file.py

--- src/run_file.py
+++ src/run_file.py
@@ -10,18 +10,34 @@ from builtins import open
 from builtins import str
 from sys import argv as args
 from sys import exit as exitexc
-from ros import main
+import ros
 import importlib
 import os.path
 # Initialise Backwards Compatibility
 from future import standard_library
 standard_library.install_aliases()
 
+# Check if file provided in execution arguments
 try:
+
+    # Try to evalute the second execution argument (file name)
     args[1]
+
+# Check for a evaluation error (file name not specified)
 except IndexError:
+
+    # Throw error
     print("ERROR: No ROS Code file provided in execution arguments")
-    print("Ensure the execution code looks something like this: python run-file.py test.ros")
+
+    # Stop execution
+    exitexc(1)
+
+
+if not os.path.isfile(args[1]):
+    print("ERROR: " + args[1] + " is not found.")
+    exitexc(1)
+    
+f = open(args[1], "r")
 
 with open(args[1]) as f:
     IGNORELINE = False
@@ -33,11 +49,17 @@ with open(args[1]) as f:
                 try:
                     importlib.import_module(
                         "ros." + value[1].split(" ")[1])
+                    break
                 except ModuleNotFoundError:
                     raise ImportError("Error: Unable to import")
+                    exitexc(1)
             elif value[1].startswith('ext'):
                 if os.path.isfile(value[1].split(" ")[1] + ".ros"):
-                    pass
+                    pass # TODO: Allow importing of external packages
+            elif value[1].startswith('imp'):
+                if os.path.isfile(value[1].split(" ")[1] + ".py"):
+                    importlib.import_module(value[1].split(" ")[1])
+            parta = value
             firstpart = value[1].split(".")[0]
             lenoffirstpart = len(value[1].split(".")[0])
             afterpart = str(value[1][lenoffirstpart + 1:])
