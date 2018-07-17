d107925fefb3170f772e3a814d08d192b7099657
Richienb <richiebendall@gmail.com>
2018-07-17 09:02:02 +0000

    CI | Tidied code files [skip ci]

--- src/sysinfo.py
+++ src/sysinfo.py
@@ -1,4 +1,9 @@
 import platform
 import sys
 
-print("ROS Code 2.0 | Running on {} {} | Python Version {}.{}.{}".format(platform.system(), platform.release(), list(sys.version_info)[0], list(sys.version_info)[1], list(sys.version_info)[2]))
+print(
+    "ROS Code 2.0 | Running on {} {} | Python Version {}.{}.{}".format(
+        platform.system(), platform.release(), list(
+            sys.version_info)[0], list(
+                sys.version_info)[1], list(
+                    sys.version_info)[2]))
