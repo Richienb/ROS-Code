6bbc1da31968b8ad9216d7d55c78f07c018b47fc
Richie Bendall <richiebendall@gmail.com>
2018-08-19 22:39:52 +1200

    Added inheritable code

--- src/ros/errors.py
+++ src/ros/errors.py
@@ -3,22 +3,32 @@ ROS Code Errors File
 This file contains all the code required for declaring errors
 """
 
+# Regular Error and code to inherit
+
+class BaseError(object):
+ 
+    def __init__(self, message):
+ 
+        Exception.__init__(message)
+ 
+        self.when = datetime.now()
+
 # Exception Conversion Error
 
 
-class ConversionError(object):
+class ConversionError(BaseError):
     pass
 
 
 # Exception Wrong Input
 
 
-class WrongInput(object):
+class WrongInput(BaseError):
     pass
 
 
 # Exception Unexpected Error
 
 
-class UnexpectedError(object):
+class UnexpectedError(BaseError):
     pass
