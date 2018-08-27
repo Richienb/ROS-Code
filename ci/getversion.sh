46d68eeee0e9cf2b9ffc9820a948dc927c5057fe
Richie Bendall <richiebendall@gmail.com>
2018-08-27 22:26:45 +1200

    Improved script

--- ci/getversion.sh
+++ ci/getversion.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-# Get the contents of line 2 of the bumpversion config
-var=$(sed '2!d' .bumpversion.cfg)
-
-# Get all the text after the 18th character of that line
-var=${var:18}
-
-# Print the output
-echo $var
+# Return the output
+echo $(sed -r -e 1d -e 's/.{17}//' -ep -eq .bumpversion.cfg)
