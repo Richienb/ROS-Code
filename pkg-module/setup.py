f21b4c32c5593d7edec156753d7d9499a35d3cc9
Richie Bendall <richiebendall@gmail.com>
2018-09-05 20:44:41 +1200

    Update install requires

--- pkg-module/setup.py
+++ pkg-module/setup.py
@@ -1,41 +1,25 @@
 # Import setuptools module
 import setuptools
 
-# Import urllib request module
-import urllib.request
-requires_file, requires_headers = urllib.request.urlretrieve(
-    "https://raw.githubusercontent.com/Richienb/ROS-Code/master/requirements.txt"
-)
-
-# Import requirements parser module
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
 # Parse the readme markdown file
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-INSTALL_REQS = parse_requirements(requires_file, session='hack')
-
-# REQS is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-REQS = [str(ir.req) for ir in INSTALL_REQS]
 
 setuptools.setup(
     name="ros-code-modules",
     version="2.0.0.16",
     author="Richie Bendall",
     author_email="richiebendall@gmail.com",
-    description=
-    "A Python module/library containing lots of tools to simplify Python development.",
+    description="A Python module/library containing lots of tools to simplify Python development.",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/Richienb/ROS-Code",
     packages=setuptools.find_packages(),
-    install_requires=REQS,
+    install_requires=[
+        'clipboard',
+        'colour',
+        'loremipsum'
+    ],
     classifiers=(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
